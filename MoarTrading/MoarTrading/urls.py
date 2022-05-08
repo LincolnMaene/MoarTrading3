75ceeec5af23c5d34234535db5045e9d9351c04d
Lincoln D Maene <maenedavidson@gmail.com>
2022-05-08 18:21:08 -0500

    heroku17

--- MoarTrading/MoarTrading/urls.py
+++ MoarTrading/MoarTrading/urls.py
@@ -18,7 +18,8 @@ from django.urls import include, path
 from django.contrib.auth.decorators import login_required    
 from less_talking_more_trading.views import home_view
 from less_talking_more_trading.views import (form_example_view, basic_order_view, basic_sell_view, Quote_query_view, Quote_view,options_view,
-options_query_view, options_data_view, Movers_Query_view, Movers_data_view, Watchlist_query_view, Market_Query_view, Market_hours_view)                                 
+options_query_view, options_data_view, Movers_Query_view, Movers_data_view, Watchlist_query_view, Market_Query_view, Market_hours_view,
+sale_trigger_sale_view)                                 
 
 
 urlpatterns = [
